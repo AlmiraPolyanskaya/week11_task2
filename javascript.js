babe6315915ba7fcea2a0f9b80bbdf367fd9e6d2
AlmiraPolyanskaya <akubagusheva@gmail.com>
2023-12-17 22:06:55 +0300

    completion commit

--- javascript.js
+++ javascript.js
@@ -1,36 +1,43 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Находим необходимые элементы DOM и записываем в переменные
-    const taskInput = document.getElementById('taskInput');
-    const addTaskBtn = document.getElementById('addTaskBtn');
-    const taskList = document.getElementById('taskList');
+// Получаем необходимые элементы DOM
+const taskInput = document.getElementById('taskInput');
+const addButton = document.getElementById('addButton');
+const taskList = document.getElementById('taskList');
 
-    // Создаем функцию createTask()
-    function createTask() {
-        // Создаем новый элемент списка
-        const newTask = document.createElement('li');
+// Обработчик события клика на кнопку "Добавить"
+addButton.addEventListener('click', function createTask() {
+    //Получаем значение, введенное в поле ввода
+    const taskText = taskInput.value;
 
-        // Присваиваем текст из поля ввода в качестве содержимого новой задачи
-        newTask.textContent = taskInput.value;
+    //Создаём новый элемент списка
+    const newTask = document.createElement('li');
 
-        // Добавляем новую задачу в список
-        taskList.appendChild(newTask);
+    //Устанавливаем текст задачи внутри созданного элемента списка
+    newTask.textContent = taskText;
 
-        // Очищаем поле ввода
-        taskInput.value = '';
+    //Добавляем созданный элемент списка в нужное место на странице
+    taskList.append(newTask);
+
+    //Очищаем поле ввода после добавления задачи
+    taskInput.value = '';
+});
+
+// Добавляем событие keypress для поля ввода
+taskInput.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        addButton.click();
     }
+});
 
-    // Создаем функцию checkTask()
-    function checkTask(event) {
-        // Проверяем, что кликнули на элемент списка
-        if (event.target.tagName === 'LI') {
-            // Переключаем состояние выполнения задачи
-            event.target.classList.toggle('completed');
+// Создайте обработчик события клика, в котором проверьте, что целевым элементом (event.target) является элемент <li>. Если это условие выполняется, то добавьте или удалите класс 'completed' у этого элемента. Задачи должны менять свой стиль, отображаясь как выполненные с зачеркнутым текстом или обратно.
+
+taskList.addEventListener('click', function checkTask(event) {
+    if (event.target.tagName === 'LI') {
+        // Код, который будет выполнен при клике на элемент <li>
+        event.target.classList.toggle('completed');
+
+        // Проверяем, содержит ли элемент класс 'completed' и выводим alert :)
+        if (event.target.classList.contains('completed')) {
+            alert('Ты молодец! Продолжай в том же духе!');
         }
     }
-
-    // Добавляем функцию-обработчик к событию клика на кнопку
-    addTaskBtn.addEventListener('click', createTask);
-
-    // Добавляем функцию-обработчик к событию клика на список задач
-    taskList.addEventListener('click', checkTask);
 });
